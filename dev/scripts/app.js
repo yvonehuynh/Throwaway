fa0e204c07f7bf4e23bbb42126270b4314c182b4
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-01-10 18:19:07 -0500

    secondary commit

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios";
+import Qs from 'qs';
+import Inputs from "./inputs";
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.searchBooze = this.searchBooze.bind(this)
+  }
+  searchBooze(e){
+    e.preventDefault();
+    const booze = document.getElementById("booze-input").value;
+    console.log(booze);
+    axios({
+      method: 'GET',
+      url: 'https://proxy.hackeryou.com',
+      dataResponse: 'json',
+      paramsSerializer: function (params) {
+        return Qs.stringify(params, { arrayFormat: 'brackets' })
+      },
+      params: {
+        reqUrl: 'http://lcboapi.com/products?access_key=MDoxNmMwYzI1YS1mNjRmLTExZTctYmE5Ni1mZjJjMWViODQ2Njg6QWtxc1ltNGVZUE9zTVhIZjg3ckR0ZFNJYUJVUndiWW4xR1BD',
+        params: {
+          q: `${booze}`
+        },
+        proxyHeaders: {
+          'header_params': 'value'
+        },
+        xmlToJSON: false
+      }
+    }).then(res => {
+      console.log(res)
+    });
+  }
     render() {
       return (
         <div>
-          Hello
+          <Inputs search={this.searchBooze}/>
         </div>
       )
     }
