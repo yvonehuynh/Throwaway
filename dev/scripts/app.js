c053090f8cb7c9d668c2d6d2dd4bdce3aafb11e3
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-02-16 23:28:19 -0500

    got stuff to dislay on the DOM rom firebase

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,4 +1,15 @@
 var Qs = require('qs');
+// const firebase = require("./firebase");
+
+var config = {
+  apiKey: "AIzaSyDyr5ZdZEHyHb5bofD0TZdO_fiU49MxO-0",
+  authDomain: "throwaway-39c5b.firebaseapp.com",
+  databaseURL: "https://throwaway-39c5b.firebaseio.com",
+  projectId: "throwaway-39c5b",
+  storageBucket: "",
+  messagingSenderId: "489478107476"
+};
+firebase.initializeApp(config);
 
 function runRequest() {
   return axios({
@@ -9,7 +20,7 @@ function runRequest() {
       return Qs.stringify(params, { arrayFormat: 'brackets' })
     },
     params: {
-      reqUrl: `https://baconipsum.com/api/?type=all-meat&sentences=3&start-with-lorem=1`,
+      reqUrl: `https://baconipsum.com/api/?type=all-meat&sentences=10&start-with-lorem=1`,
       xmlToJSON: false
     }
   }).then((result) => {
@@ -25,40 +36,30 @@ function runRequest() {
 
 // function that takes the API data and turns it into a random username
 const getUserName = (res) => {
-  let array = randomizeWords(res)
+  let array = randomizeWords(res);
+  array = array.replace(/,/, "");
   let para = document.getElementById("username");
   para.innerHTML = array;
-  console.log("username: ", para)
+  console.log("username: ", array);
   return array;
 }
 
 // function to generate randomized password
 const getPassword = (res) => {
-  let password = randomizeWords(res)
-  password = password.charAt(0).toUpperCase() + password.slice(1)
+  let password = randomizeWords(res);
+  password = password.charAt(0).toUpperCase() + password.slice(1);
   let specialChar = '!?=#*$@+-.';
-  const index = Math.floor(Math.random() * specialChar.length)
-  specialChar = specialChar[index]
-  password = password + specialChar
-  let para = document.getElementById("password");
-  para.innerHTML = password;
-  console.log("password: ", password)
-  // let password = getUserName(res);
-  // // take word and make first letter to uppercase
-  // password.charAt(0).toUpperCase() + password.slice(1)
-  // let specialChar = '!?=#*$@+-.';
-  // const index = Math.floor(Math.random() * specialChar.length)
-  // // get random specialChar
-  // specialChar = specialChar[index]
-  // // add specialChar to new password
-  // password = password + specialChar
-  // // if password is less than 8 characters, rerun function
-  // if (password.length < 8) {
-  //   getPassword()
-  // } else {
-  //   console.log("password: ", password)
-  //   return password;
-  // }
+  const index = Math.floor(Math.random() * specialChar.length);
+  specialChar = specialChar[index];
+  password = password + specialChar;
+  if (password.length < 8) {
+    getPassword();
+  } else {
+    let para = document.getElementById("password");
+    para.innerHTML = password;
+    console.log("password: ", password)
+    return password
+  }
 }
 
 const randomizeWords=(res)=>{
@@ -67,16 +68,49 @@ const randomizeWords=(res)=>{
   // turn words in the sentence to separate strings in an array
   const randomItemtoString = randomItem.split(" ");
   // first word: get random word in the array
-  const randomWord = randomItemtoString[Math.floor(Math.random() * randomItemtoString.length)]
+  const randomWord = randomItemtoString[Math.floor(Math.random() * randomItemtoString.length)];
   // second word: get second random word in array
-  const secondWord = randomItemtoString[Math.floor(Math.random() * randomItemtoString.length)]
+  const secondWord = randomItemtoString[Math.floor(Math.random() * randomItemtoString.length)];
   // generate a random number up to 1000
   const randomNum = Math.floor(Math.random() * 1000)
   // combine first word, second word, and random number to create random username
-  let password = randomWord + secondWord + randomNum;
-  password.replace(/.,/, "");
-  return password
+  let randomizedWord = randomWord + secondWord + randomNum;
+  randomizedWord.replace(/.,/, "");
+  return randomizedWord;
 }
 
+document.getElementById("generateUsername").addEventListener("click", function(e){
+  e.preventDefault();
+  runRequest();
+})
 
-runRequest();
+document.getElementById("save").addEventListener("click", function(e){
+  e.preventDefault();
+ // console.log("boomboom: ", username, "password: ", password);
+ const username = document.getElementById("username").innerHTML;
+ const password = document.getElementById("password").innerHTML;
+  const throwawayDetails={
+    username,
+    password
+  }
+  const dbRef = firebase.database().ref();
+  dbRef.push(throwawayDetails)
+})
+
+displayFav();
+function displayFav(){
+  const dbRef = firebase.database().ref();
+
+  dbRef.on("value", (firebaseData) => {
+    let accounts = [];
+    const accountData = firebaseData.val();
+    for (let itemKey in accountData) {
+      accountData[itemKey].key = itemKey;
+      accounts.push(accountData[itemKey])
+      const password = accountData[itemKey]["password"];
+      const user = accountData[itemKey]["username"];
+      console.log("user: ", user, "password: ", password)
+    }
+  });
+
+}
---
+++ dev/scripts/firebase.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyDyr5ZdZEHyHb5bofD0TZdO_fiU49MxO-0",
+    authDomain: "throwaway-39c5b.firebaseapp.com",
+    databaseURL: "https://throwaway-39c5b.firebaseio.com",
+    projectId: "throwaway-39c5b",
+    storageBucket: "",
+    messagingSenderId: "489478107476"
+  };
+  firebase.initializeApp(config);
