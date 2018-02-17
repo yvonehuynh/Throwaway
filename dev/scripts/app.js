9c516b10e220bf48644bc9f6fe3178580affbd99
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-02-16 19:22:59 -0500

    acheived random word selection

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -9,14 +9,15 @@ function runRequest() {
       return Qs.stringify(params, { arrayFormat: 'brackets' })
     },
     params: {
-      reqUrl: `https://baconipsum.com/api/?type=all-meat&start-with-lorem=1`,
+      reqUrl: `https://baconipsum.com/api/?type=all-meat&sentences=3&start-with-lorem=1`,
       xmlToJSON: false
     }
   }).then((result) => {
     const res = result.data;
     var randomItem = res[Math.floor(Math.random() * res.length)];
     const randomItemtoString = randomItem.split(" ");
-    console.log(randomItemtoString)
+    const randomWord = randomItemtoString[Math.floor(Math.random() * randomItemtoString.length)]
+    console.log(randomWord)
   })
     .catch((error) => {
       swal("Oops!", "This is not a levidrome as one of the words in not valid.", "error");
