53330aac187eac694d33b3a16404b575cb69c63d
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-02-16 22:16:24 -0500

    added both username and password to DOM

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -16,6 +16,7 @@ function runRequest() {
     const res = result.data;
     getUserName(res);
     getPassword(res);
+    return res;
   })
     .catch((error) => {
       alert("Oops!");
@@ -24,6 +25,43 @@ function runRequest() {
 
 // function that takes the API data and turns it into a random username
 const getUserName = (res) => {
+  let array = randomizeWords(res)
+  let para = document.getElementById("username");
+  para.innerHTML = array;
+  console.log("username: ", para)
+  return array;
+}
+
+// function to generate randomized password
+const getPassword = (res) => {
+  let password = randomizeWords(res)
+  password = password.charAt(0).toUpperCase() + password.slice(1)
+  let specialChar = '!?=#*$@+-.';
+  const index = Math.floor(Math.random() * specialChar.length)
+  specialChar = specialChar[index]
+  password = password + specialChar
+  let para = document.getElementById("password");
+  para.innerHTML = password;
+  console.log("password: ", password)
+  // let password = getUserName(res);
+  // // take word and make first letter to uppercase
+  // password.charAt(0).toUpperCase() + password.slice(1)
+  // let specialChar = '!?=#*$@+-.';
+  // const index = Math.floor(Math.random() * specialChar.length)
+  // // get random specialChar
+  // specialChar = specialChar[index]
+  // // add specialChar to new password
+  // password = password + specialChar
+  // // if password is less than 8 characters, rerun function
+  // if (password.length < 8) {
+  //   getPassword()
+  // } else {
+  //   console.log("password: ", password)
+  //   return password;
+  // }
+}
+
+const randomizeWords=(res)=>{
   // get random array from API results (API results return different arrays)
   const randomItem = res[Math.floor(Math.random() * res.length)];
   // turn words in the sentence to separate strings in an array
@@ -35,32 +73,10 @@ const getUserName = (res) => {
   // generate a random number up to 1000
   const randomNum = Math.floor(Math.random() * 1000)
   // combine first word, second word, and random number to create random username
-  let array = randomWord + secondWord + randomNum;
-  array.replace(/.,/, "");
-  let para = document.getElementById("username");
-  para.innerHTML = array;
-  console.log("username: ", para)
-  return array;
+  let password = randomWord + secondWord + randomNum;
+  password.replace(/.,/, "");
+  return password
 }
 
-// function to generate randomized password
-const getPassword = (res) => {
-  let password = getUserName(res);
-  // take word and make first letter to uppercase
-  password.charAt(0).toUpperCase() + password.slice(1)
-  let specialChar = '!?=#*$@+-.';
-  const index = Math.floor(Math.random() * specialChar.length)
-  // get random specialChar
-  specialChar = specialChar[index]
-  // add specialChar to new password
-  password = password + specialChar
-  // if password is less than 8 characters, rerun function
-  if (password.length < 8) {
-    getPassword()
-  } else {
-    console.log("password: ", password)
-    return password;
-  }
-}
 
 runRequest();
