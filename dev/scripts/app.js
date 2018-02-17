47e90c0185a208c62c1c2dd0f8d12bcf0490ffb6
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-02-16 20:10:50 -0500

    added lots of comments

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -13,11 +13,13 @@ function runRequest() {
       xmlToJSON: false
     }
   }).then((result) => {
-    const res = result.data;
-    getUserName(res);
+      const res = result.data;
+      return res
+      getUserName(res);
+      getPassword(res); 
   })
     .catch((error) => {
-      swal("Oops!", "This is not a levidrome as one of the words in not valid.", "error");
+      alert("Oops!");
     });
 }
 
@@ -25,12 +27,38 @@ runRequest();
 
 // function that takes the API data and turns it into a random username
 const getUserName=(res)=>{
+  // get random array from API results (API results return different arrays)
   const randomItem = res[Math.floor(Math.random() * res.length)];
+  // turn words in the sentence to separate strings in an array
   const randomItemtoString = randomItem.split(" ");
+  // first word: get random word in the array
   const randomWord = randomItemtoString[Math.floor(Math.random() * randomItemtoString.length)]
+  // second word: get second random word in array
   const secondWord = randomItemtoString[Math.floor(Math.random() * randomItemtoString.length)]
+  // generate a random number up to 1000
   const randomNum = Math.floor(Math.random() * 1000)
+  // combine first word, second word, and random number to create random username
   let array = randomWord + secondWord + randomNum;
   array.replace(/.,/, "")
-  console.log(array)
+  return array;
+ // console.log(array)
+}
+
+// function to generate randomized password
+const getPassword=(res)=>{
+  let password = getUserName(res);
+  // take word and make first letter to uppercase
+  password.charAt(0).toUpperCase() + password.slice(1)
+  let specialChar = '!?=#*$@+-.';
+  const index = Math.floor(Math.random()* specialChar.length)
+  // get random specialChar
+  specialChar = specialChar[index]
+  // add specialChar to new password
+  password = password+specialChar
+  // if password is less than 8 characters, rerun function
+  if (password.length < 8) {
+    getPassword()
+  } else {
+    return password;
+  }
 }
