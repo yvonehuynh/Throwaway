6d5a7f857c5cedda0bccc4d11f966333b4a1a030
Yvone Huynh <sw.yvone.huynh@gmail.com>
2018-02-16 19:20:41 -0500

    changed api

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -9,37 +9,18 @@ function runRequest() {
       return Qs.stringify(params, { arrayFormat: 'brackets' })
     },
     params: {
-      reqUrl: `https://od-api.oxforddictionaries.com:443/api/v1/entries/en/bomb/synonyms;antonyms`,
-      proxyHeaders: {
-        'header_params': 'value',
-        "Accept": "application/json",
-        "app_id": "8ec64674",
-        "app_key": "b5a5e3f12d46fc718b916e1aaa1c3509"
-      },
+      reqUrl: `https://baconipsum.com/api/?type=all-meat&start-with-lorem=1`,
       xmlToJSON: false
     }
   }).then((result) => {
-       const synonym = result.data.results.map(res=>{
-         return res.lexicalEntries.map(secondResult=>{
-           return secondResult.entries.map(thirdResult=>{
-               return thirdResult.senses.map(fourthRes=>{
-                 return fourthRes.synonyms.map(fifthRes=>{
-                  turnArray(fifthRes.id)
-                  return fifthRes.id;
-              })
-            })
-          })
-        })
-      });  
+    const res = result.data;
+    var randomItem = res[Math.floor(Math.random() * res.length)];
+    const randomItemtoString = randomItem.split(" ");
+    console.log(randomItemtoString)
   })
     .catch((error) => {
       swal("Oops!", "This is not a levidrome as one of the words in not valid.", "error");
     });
-}
-const arrays = [];
-function turnArray(list) {
-  arrays.push(list)
-  console.log(arrays)
 }
 
 runRequest();
